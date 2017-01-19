cc54e9f339128c54fd6acdae181c24a1bf227b53
Somenath Sinha <somenath.article@gmail.com>
2017-01-19 13:37:04 +0530

    Removed Explanation

--- src/ConVow.java
+++ src/ConVow.java
@@ -3,9 +3,6 @@
  * INPUT : 	MAN IS MORTAL
  * OUTPUT:	MNA SI MRTLOA
  * (First consonants, then vowels without changing the sequence)
- * 
- * M A N   I S   M O R T  A  L
- * 0 1 2 3 4 5 6 7 8 9 10 11 12
  */
 
 import java.io.*;
