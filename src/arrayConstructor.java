ebd7435cb1b0c7856fe9033b67e1cf1959e88e5f
Somenath Sinha <somenath.article@gmail.com>
2017-01-28 12:13:25 +0530

    Showed how to pass an array to a function/constructor

---
+++ src/arrayConstructor.java
@@ -0,0 +1,25 @@
+import java.io.*;
+public class arrayConstructor 
+{
+	int i[];
+	arrayConstructor(int arr[])
+	{
+		i=arr;
+	}
+	void display()
+	{
+		for(int j=0;j<10;j++)
+			System.out.print(i[j]+"\t");
+	}
+	public static void main(String args[]) throws IOException
+	{
+		int m[] = new int[10];
+		System.out.println("Enter the array : ");
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		for(int j=0;j<10;j++)
+			m[j] = Integer.parseInt(in.readLine());
+		
+		arrayConstructor obj = new arrayConstructor(m);
+		obj.display();
+	}
+}
